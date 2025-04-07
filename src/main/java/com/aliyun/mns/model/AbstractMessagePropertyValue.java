114bdfb49aff71e233114911628cd686fa8241ee
浩龙 <liuhaolong.lhl@alibaba-inc.com>
2025-04-07 18:13:01 +0800

    refactor: 移除代码中的作者和日期注释

--- src/main/java/com/aliyun/mns/model/AbstractMessagePropertyValue.java
+++ src/main/java/com/aliyun/mns/model/AbstractMessagePropertyValue.java
@@ -2,7 +2,6 @@ package com.aliyun.mns.model;
 
 /**
  * @author haolong
- * @date 2025/2/24 17:19
  */
 public interface AbstractMessagePropertyValue {
     String getStringValueByType();
--- src/main/java/com/aliyun/mns/model/SystemPropertyType.java
+++ src/main/java/com/aliyun/mns/model/SystemPropertyType.java
@@ -2,7 +2,6 @@ package com.aliyun.mns.model;
 
 /**
  * @author haolong
- * @date 2025/2/24 16:55
  */
 public enum SystemPropertyType {
     /**
